b999a718c5592392f7372d616d006e3bb7d18168
Tunous <lukus178@gmail.com>
2015-08-27 16:06:22 +0200

    Add commands
    Commands:
     - !start - Start new round
     - !stop  - Stop running round
     - !on    - Enable round manager
     - !off   - Disable round manager

---
+++ BotBitsExt.Rounds/Commands.cs
@@ -0,0 +1,85 @@
+﻿using System;
+using BotBits;
+using BotBits.Commands;
+using BotBits.Permissions;
+
+namespace BotBitsExt.Rounds
+{
+    internal sealed class Commands : Package<Commands>
+    {
+        private RoundsManager roundsManager;
+        private bool requireModerator;
+
+        public Commands()
+        {
+            InitializeFinish += (sender, e) =>
+            {
+                roundsManager = RoundsManager.Of(BotBits);
+
+                if (CommandsExtension.IsLoadedInto(BotBits))
+                {
+                    CommandLoader.Of(BotBits).Load(this);
+
+                    requireModerator = PermissionsExtension.IsLoadedInto(BotBits);
+                }
+            };
+        }
+
+        private void RequireModerator(IInvokeSource source)
+        {
+            if (requireModerator)
+                Group.Moderator.RequireFor(source);
+        }
+
+        [Command(0, "start")]
+        private void StartCommand(IInvokeSource source, ParsedRequest request)
+        {
+            RequireModerator(source);
+
+            source.Reply("Starting new round...");
+            roundsManager.ForceStart();
+        }
+
+        [Command(0, "stop")]
+        private void StopCommand(IInvokeSource source, ParsedRequest request)
+        {
+            RequireModerator(source);
+
+            source.Reply("Stopping round...");
+            roundsManager.ForceStop();
+        }
+
+        [Command(0, "on")]
+        private void OnCommand(IInvokeSource source, ParsedRequest request)
+        {
+            RequireModerator(source);
+
+            if (roundsManager.Enabled)
+            {
+                source.Reply("Bot is already enabled.");
+            }
+            else
+            {
+                source.Reply("Enabling bot...");
+                roundsManager.Enabled = true;
+            }
+        }
+
+        [Command(0, "off")]
+        private void OffCommand(IInvokeSource source, ParsedRequest request)
+        {
+            RequireModerator(source);
+
+            if (!roundsManager.Enabled)
+            {
+                source.Reply("Bot is already disabled.");
+            }
+            else
+            {
+                source.Reply("Disabling bot...");
+                roundsManager.Enabled = false;
+            }
+        }
+    }
+}
+
